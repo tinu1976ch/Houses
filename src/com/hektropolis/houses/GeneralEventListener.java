1303cd361f6698d631984ee729b8d653dc4c7e63
CullanP <cullanpage@gmail.com>
2015-02-09 19:57:54 -0800

    Update GeneralEventListener.java

--- src/com/hektropolis/houses/GeneralEventListener.java
+++ src/com/hektropolis/houses/GeneralEventListener.java
@@ -2,21 +2,16 @@ package com.hektropolis.houses;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import org.bukkit.Material;
-import org.bukkit.block.Sign;
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 
-import com.hektropolis.houses.signs.HouseSign;
-import com.hektropolis.houses.signs.RentSign;
+import com.hektropolis.houses.database.DatabaseQuery;
 
 public class GeneralEventListener implements Listener {
 
@@ -25,14 +20,21 @@ public class GeneralEventListener implements Listener {
 	public GeneralEventListener(Houses plugin) {
 		this.plugin = plugin;
 	}
-
+	
 	@EventHandler
 	public void onJoinCheck(PlayerJoinEvent e) {
-		final Player p = e.getPlayer();
-		Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+			final Player p = e.getPlayer();
+		for (int houseClass : DatabaseQuery.getClasses("rentals")) {
+		for (int houseNumber : DatabaseQuery.getNumbers("rentals", houseClass)) {
+			DatabaseQuery DBQuery = new DatabaseQuery(p.getWorld().getName(), houseClass, houseNumber);
+		if(DBQuery.playerHasRental(p.getName())) {
+			int delay = (int) plugin.getConfig().getDouble("expired-check-timer") * 20;
+			Bukkit.broadcastMessage(Integer.toString(delay));
+			Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
 			@Override
 			public void run() {
 				try {
+				Bukkit.broadcastMessage("test");
 				ResultSet rs = Houses.sqlite.query("SELECT * FROM rentals WHERE player='" + p.getName() + "'");
 				while (rs.next()) {
 					p.sendMessage(ChatColor.translateAlternateColorCodes('&', Houses.prefix + "&6Your rental at class &2" + rs.getInt("class") + "&6 number &2" + rs.getInt("number") + 
@@ -41,8 +43,11 @@ public class GeneralEventListener implements Listener {
 				rs.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
+							}
+						}
+					}, delay);
 				}
 			}
-		}, 1);
+		}
 	}
 }
